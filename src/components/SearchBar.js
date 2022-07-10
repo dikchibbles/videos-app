91772ebb56ce79134fc60e5995d6632142511754
Leo <bredleff@yandex.com>
2022-07-10 15:43:30 +0300

    modify .gitignore and change the SearchBar title text to be search for a video

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,7 +12,7 @@ const SearchBar = ({ onSubmit }) => {
         <div className="search-bar ui segment">
             <form className="ui form" onSubmit={onFormSubmit}>
                 <div className="field">
-                    <label>Search Video</label>
+                    <label>Search for a video</label>
                     <input type="text" value={term} onChange={(e) => setTerm(e.target.value)} />
                 </div>
             </form>
